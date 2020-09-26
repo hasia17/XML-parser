c69abd8f59dd71c73f40c926a9a1b48a42cbb13f
hasia17 <aaggaattaa.wan@gmail.com>
2020-09-26 19:46:37 +0200

    update

--- src/xmlparser/Runner.java
+++ src/xmlparser/Runner.java
@@ -1,33 +1,28 @@
 
 package xmlparser;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
-import java.util.ArrayList;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+
 import org.xml.sax.SAXException;
 
-
 public class Runner {
-    
-    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
-        
-        //for(String a : args) {
-        
-        //String url = a;
-        XMLParser parser = new XMLParser("https://www.w3schools.com/xml/cd_catalog.xml", "myFile.xml", "myFile.csv", "myFile.xlsx");
-        parser.run();
 
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
+
+        XMLParser parser = new XMLParser(
+                "https://www.w3schools.com/xml/cd_catalog.xml",
+                "myFile.xml",
+                "myFile.csv",
+                "myFile.xlsx"
+        );
+//         XMLParser parser = new XMLParser(
+//            args[0] ,
+//            args[1],
+//            args[2],
+//            args[3]
+//         );
+
+        parser.run();
     }
 }
